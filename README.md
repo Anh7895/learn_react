ac004c1e6de7764452996cfd0f32463baa173632
quang anh <truongquanganh0095@gmail.com>
2023-11-03 15:06:46 +0700

    init

--- README.md
+++ README.md
@@ -68,3 +68,14 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `npm run build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+
+
+### dependentce
+1. ant
+npm install antd --save
+
+npm install @ant-design/icons --save
+import { GoogleOutlined } from '@ant-design/icons';
+
+tạo <GoogleOutlined/>
--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
+import { Button, Divider, Form, Input, Typography, message } from 'antd';
+import React from 'react';
 import './App.css';
+import FormItem from 'antd/es/form/FormItem';
+import { FacebookOutlined, GoogleOutlined, TwitterOutlined } from '@ant-design/icons';
 
 function App() {
+  const login = () => {
+    message.success('Login success');
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="appBg">
+      <Form className='loginForm' onFinish={login}>
+        <Typography.Title>Well come</Typography.Title>
+        <FormItem rules={[{
+          required: true,
+          type: 'email',
+          message: 'Please enter valid email',
+        },]} label='Email' name={'myEmail'}>
+          <Input placeholder="email" />
+        </FormItem>
+        <FormItem rules={[{
+          required: true,
+
+          message: 'Please enter valid password',
+        },]} label='Password' name={'myPassword'}>
+          <Input.Password placeholder="password" />
+        </FormItem>
+        <Button type='primary' htmlType='submit' block>Login</Button>
+        <Divider style={{ borderColor: 'black' }}>
+          or Login with
+        </Divider>
+        <div className='socialLogin'>
+          <GoogleOutlined className='socialIcon' onClick={login} style={{ color: 'red' }} />
+          <FacebookOutlined className='socialIcon' onClick={login} style={{ color: 'blue' }} />
+          <TwitterOutlined className='socialIcon' onClick={login} style={{ color: 'cyan' }} />
+        </div>
+      </Form>
     </div>
   );
 }
